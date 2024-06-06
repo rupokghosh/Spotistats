eaf167a78d86a2ea6680dc4e6b2e1f5c02e8fa19
rupokghosh <rupokghosh291@gmail.com>
2024-06-06 14:14:22 -0700

    add footer

---
+++ app/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex justify-center items-center gap-10 p-10">
+      <div className="text-md text-secondary">A project by Rupok Adin ©</div>
+      <a
+        href="https://github.com/rupokghosh/Spotistats"
+        className="text-secondary underline"
+      >
+        Github
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
