cd63cdfc1c2e37d1069223084a33f64a7dcaa0f4
rupokghosh <rupokghosh291@gmail.com>
2024-06-07 11:54:57 -0700

    add Links to header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,13 +1,19 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
   return (
     <div className="py-8 px-24 flex align-items justify-between">
-      <h1 className="font-bold text-2xl text-secondary">Spotistats</h1>
+      <Link href="/" className="font-bold text-2xl text-secondary">
+        Spotistats
+      </Link>
       <div className="flex gap-8 align-items justify-between text-accent">
-        <button className="rounded-full border-b border-accent px-4 hover:bg-neutral">
+        <Link
+          href="/stats"
+          className="rounded-full border-b border-accent px-4 hover:bg-neutral"
+        >
           myStats
-        </button>
+        </Link>
         <button className="rounded-full border-b border-accent px-4 hover:bg-neutral ">
           Share
         </button>
