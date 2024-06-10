c1c37e0118afbda26a0a8f4b833d0eda7a4ca778
rupokghosh <rupokghosh291@gmail.com>
2024-06-10 13:23:17 -0700

    fix layout to display artists

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,6 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const { data: session } = useSession();
-  console.log(session);
 
   const [topArtists, setTopArtists] = useState([]);
   const [topTracks, setTopTracks] = useState([]);
@@ -20,7 +19,6 @@ export default function Home() {
             Authorization: `Bearer ${session.token.access_token}`,
           };
 
-          console.log("Headers:", headers);
           const [artistsResponse, tracksResponse] = await Promise.all([
             axios.get(
               "https://api.spotify.com/v1/me/top/artists?limit=5&time_range=short_term",
@@ -42,13 +40,17 @@ export default function Home() {
       fetchTopItems();
     }
   }, [session]);
+
+  console.log(topTracks);
   const img =
     "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg";
 
   return (
     <div className="flex flex-col items-center justify-evenly gap-10 mt-12">
       <img src={img} alt="avatar" className="w-24 rounded-full" />
-      <div className="font-bold text-2xl text-neutral">Top Songs</div>
+      <div className="font-bold text-2xl text-neutral">
+        Top Songs this month{" "}
+      </div>
       <div className="songs flex justify-evenly items-center gap-6 ">
         <ul>
           {topTracks.map((track) => (
@@ -56,11 +58,23 @@ export default function Home() {
           ))}
         </ul>
       </div>
-      <div className="font-bold text-2xl text-neutral"> Top Artists </div>
+      <div className="font-bold text-2xl text-neutral">
+        {" "}
+        Top Artists this month{" "}
+      </div>
       <div className="artists flex justify-evenly items-center gap-6">
-        <ul>
+        <ul className="flex items-center justify-evenly gap-10">
           {topArtists.map((artist) => (
-            <li key={artist.id}>{artist.name}</li>
+            <li key={artist.id}>
+              <div className="flex flex-col gap-8 justify-between items-center">
+                <img
+                  src={artist.images[0].url}
+                  alt={artist.name}
+                  className="w-24 rounded-full"
+                />
+                <p className="font-bold text-lg">{artist.name}</p>
+              </div>
+            </li>
           ))}
         </ul>
       </div>
