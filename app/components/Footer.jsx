c587360ae0f5fde609ce51901d75fd666d817b64
rupokghosh <rupokghosh291@gmail.com>
2024-06-13 00:44:06 -0700

    update footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,14 +2,17 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex justify-center items-center gap-10 p-10">
-      <div className="text-md text-secondary">A project by Rupok Adin ©</div>
-      <a
-        href="https://github.com/rupokghosh/Spotistats"
-        className="text-secondary underline"
-      >
-        Github
-      </a>
+    <div className="flex flex-col items-center justify-center p-2">
+      <p>&copy; 2024 Statsify. All rights reserved.</p>
+      <div className="flex justify-center items-center gap-10 p-8">
+        <div className="text-md text-secondary">A project by Rupok Adin </div>
+        <a
+          href="https://github.com/rupokghosh/Spotistats"
+          className="text-secondary underline"
+        >
+          Github
+        </a>
+      </div>
     </div>
   );
 };
