40ac9df74f9c3061ab6fc23f1bbeca5f780e0ee3
rupokghosh <rupokghosh291@gmail.com>
2024-06-29 11:21:47 -0700

    add testimonials

--- app/page.jsx
+++ app/page.jsx
@@ -2,18 +2,19 @@
 
 import React from "react";
 import Link from "next/link";
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession, signIn } from "next-auth/react";
 import Footer from "./components/Footer";
+import Testimonials from "./components/Testimonials";
 
 const Pages = () => {
   const { data: session } = useSession();
+
   return (
     <div className="flex flex-col justify-center items-center gap-10">
       <h1 className="text-2xl font-bold text-secondary mt-32">Statsify</h1>
-      <h3 className="text-lg text-neutral text-center ">
-        Discover Your Spotify Stats and Get insights into your listening habits
-        <br />
-        and enjoy personalized music analytics.
+      <h3 className="text-lg text-accent text-center ">
+        Why wait for spotify wrapped once a year? <br />
+        Discover Your Spotify Stats and Get insights into your listening habits.
       </h3>
       <button
         className="rounded-full py-2 px-4 mb-32 border border-neutral hover:bg-neutral text-neutral hover:text-primary-content "
@@ -21,6 +22,7 @@ const Pages = () => {
       >
         Get Started
       </button>
+      <Testimonials />
       <Footer />
     </div>
   );
