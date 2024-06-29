72672bafa587cdd2be7a44ca29b5293b9ae16666
rupokghosh <rupokghosh291@gmail.com>
2024-06-29 11:24:23 -0700

    minor changes

--- app/page.jsx
+++ app/page.jsx
@@ -11,17 +11,20 @@ const Pages = () => {
 
   return (
     <div className="flex flex-col justify-center items-center gap-10">
-      <h1 className="text-2xl font-bold text-secondary mt-32">Statsify</h1>
+      <h1 className="text-2xl font-bold text-secondary mt-48">Statsify</h1>
       <h3 className="text-lg text-accent text-center ">
         Why wait for spotify wrapped once a year? <br />
         Discover Your Spotify Stats and Get insights into your listening habits.
       </h3>
       <button
-        className="rounded-full py-2 px-4 mb-32 border border-neutral hover:bg-neutral text-neutral hover:text-primary-content "
+        className="rounded-full py-2 px-4 mb-24 border border-neutral hover:bg-neutral text-neutral hover:text-primary-content "
         onClick={() => signIn("spotify", { callbackUrl: "/home" })}
       >
         Get Started
       </button>
+      <h1 className="text-center font-bold text-secondary text-xl">
+        What our users have to say about us
+      </h1>
       <Testimonials />
       <Footer />
     </div>
